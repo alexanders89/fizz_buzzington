0c9408caf6149e5523d571ad0a41291d3bd871a2
Alex <alex.satur89@gmail.com>
2017-10-27 16:35:10 +0100

    removed hardcoded sections and passing with 3 and 5

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -8,4 +8,27 @@ describe('Fizzbuzz', function() {
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
   });
+
+  describe('knows when a number is not', function(){
+    it('divisible by 3', function(){
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+    });
+  });
+
+  describe('knows when a number is', function(){
+    it('divisible by 5', function(){
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
+  });
+
+  describe('knows when a number is not', function(){
+    it('divisible by 5', function(){
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.isDivisibleByFive(4)).toBe(false);
+    });
+  });
+
+
 });
