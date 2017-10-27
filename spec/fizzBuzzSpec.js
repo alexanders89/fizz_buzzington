21a359351d101d79cf462fe2af5aa1cc41a648d0
Alex <alex.satur89@gmail.com>
2017-10-27 17:07:53 +0100

    all tests green

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -2,33 +2,70 @@ describe('Fizzbuzz', function() {
 
   var fizzbuzz;
 
+  beforeEach(function() {
+    fizzbuzz = new Fizzbuzz();
+  });
+
   describe('knows when a number is', function(){
     it('divisible by 3', function(){
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
   });
 
   describe('knows when a number is not', function(){
     it('divisible by 3', function(){
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
     });
   });
 
   describe('knows when a number is', function(){
     it('divisible by 5', function(){
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
     });
   });
 
   describe('knows when a number is not', function(){
     it('divisible by 5', function(){
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByFive(4)).toBe(false);
     });
   });
+
+  describe('knows when a number is', function(){
+    it('divisible by 15', function(){
+      expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true);
+    });
+  });
+
+  describe('knows when a number is not', function(){
+    it('divisible by 15', function(){
+      expect(fizzbuzz.isDivisibleByFifteen(14)).toBe(false);
+    });
+  });
+
+  describe('when playing, says', function(){
+    it('"Fizz" when a number is divisible by 3', function(){
+      expect(fizzbuzz.says(3)).toEqual("Fizz")
+    });
+  });
+
+  describe('when playing, says', function(){
+    it('"Buzz" when a number is divisible by 5', function(){
+      expect(fizzbuzz.says(5)).toEqual("Buzz")
+    });
+  });
+
+  describe('when playing, says', function(){
+    it('"FizzBuzz" when a number is divisible by 15', function(){
+      expect(fizzbuzz.says(15)).toEqual("FizzBuzz")
+    });
+  });
+
+  describe('when playing, says', function(){
+    it('returns the number if not divisible', function(){
+      expect(fizzbuzz.says(4)).toEqual(4)
+    });
+  });
+
 
 
 });
